e41d6a4cc9f5fa1eadf8b2a8571708ce13a5ea0a
jmarinr <jmarinr@users.noreply.github.com>
2025-11-11 21:14:34 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Custom domain en raíz => base '/'
 export default defineConfig({
   plugins: [react()],
-  base: '/',            // <-- clave para mvp-ot-crm.henkancx.com
-  build: { outDir: 'dist' },
-});
+  base: '/',
+})
